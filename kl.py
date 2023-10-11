53537eb3d4c669b2bca328e0fb56e572c2022658
kamikazizen <bor.astat@gmail.com>
2023-10-11 13:21:42 +0300

    prompt fix, memory fix

--- kl.py
+++ kl.py
@@ -1,6 +1,7 @@
 from transformers import (
     AutoTokenizer, 
-    AutoModelForCausalLM
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
 )
 from transformers import set_seed
 import torch
@@ -12,43 +13,34 @@ import time
 import json
 
 
-logger = logging.getLogger(__name__)
-
 parser = ArgumentParser()
 parser.add_argument('--model1', type=str, required=True)
 parser.add_argument('--model2', type=str, required=True)
 parser.add_argument('--seq-length', type=int, required=True)
 parser.add_argument('--num-samples', type=int, required=True)
-parser.add_argument('--prompt', type=str, default='', required=False)
-parser.add_argument('--prompt2', type=str, default=None, required=False)
+parser.add_argument('--prompt1', type=str, default='', required=False)
+parser.add_argument('--prompt2', type=str, default='', required=False)
+parser.add_argument('--system1', type=str, default='', required=False)
+parser.add_argument('--system2', type=str, default='', required=False)
+
+LLAMA2_INSTR_TEMPLATE = """[INST] <<SYS>>
+{} 
+<</SYS>>
+
+{} [/INST]""" 
 
 
-def generate(model, prompt_text, num_seq, seq_length):
-    tokenizer = AutoTokenizer.from_pretrained(model.name_or_path,
-                                              device_map='auto',
-                                              padding_side="left",
-                                              # Needed for HF name change
-                                              tokenizer_type='llama' if 'llama' in model.name_or_path else None, 
-                                              add_bos_token=True,
-                                              legacy=False,
-                                              use_fast=False)
-    print('generation prompt text:', prompt_text)
-    prompt = tokenizer(prompt_text, return_tensors="pt").to(model.device)
+def modify_prompt(model_name, prompt_text, system_text):
+    model_name = model_name.lower()
 
-    model.eval()
-    with torch.no_grad():
-        outputs = model.generate(**prompt, do_sample=True, 
-                                 min_new_tokens=seq_length, max_new_tokens=seq_length,
-                                 num_return_sequences=num_seq)
-        
-    # checking that number of generated tokens is equal to seq_length
-    assert len(outputs[0]) == len(prompt['input_ids'][0]) + seq_length
-        
-    texts = [tokenizer.decode(o, skip_special_tokens=True) for o in outputs]
-    return texts
+    # add system prompt and instruction tokens if model supports instructions
+    if 'llama-2' in model_name and 'chat' in model_name: 
+        prompt_text = LLAMA2_INSTR_TEMPLATE.format(system_text, prompt_text)
+
+    return prompt_text
 
 
-def get_log_sum(model, texts, prompt_text):
+def generate(model, num_seq, seq_length, prompt_text, system_text):
     tokenizer = AutoTokenizer.from_pretrained(model.name_or_path,
                                               device_map='auto',
                                               padding_side="left",
@@ -57,12 +49,49 @@ def get_log_sum(model, texts, prompt_text):
                                               add_bos_token=True,
                                               legacy=False,
                                               use_fast=False)
+    # check that bos token is added
+    assert len(tokenizer.encode('')) == 1
     
+    prompt_text = modify_prompt(model.name_or_path, prompt_text, system_text)
+    print('Generation prompt:', prompt_text)
+
+    prompt = tokenizer(prompt_text, return_tensors="pt").to(model.device)
+    num_prompt_tokens = len(prompt['input_ids'][0])
+
+    model.eval()
+    with torch.no_grad():
+        outputs = model.generate(**prompt, do_sample=True, 
+                                 min_new_tokens=seq_length, max_new_tokens=seq_length,
+                                 num_return_sequences=num_seq)
+        
+    # checking that number of new generated tokens is equal to seq_length
+    assert len(outputs[0]) == num_prompt_tokens + seq_length
+        
+    # removing prompt tokens and converting to text
+    texts = [tokenizer.decode(o[num_prompt_tokens:], skip_special_tokens=True) for o in outputs]
+
+    torch.cuda.empty_cache()
+
+    return texts
+
+
+def get_log_sum(model, texts, prompt_text, system_text):
+    tokenizer = AutoTokenizer.from_pretrained(model.name_or_path,
+                                              device_map='auto',
+                                              padding_side="left",
+                                              # Needed for HF name change?
+                                              tokenizer_type='llama' if 'llama' in model.name_or_path else None, 
+                                              add_bos_token=True,
+                                              legacy=False,
+                                              use_fast=False)
     # check that first token is bos
     assert len(tokenizer.encode('')) == 1
+    
+    prompt_text = modify_prompt(model.name_or_path, prompt_text, system_text)
+    print('Log calculation prompt:', prompt_text)
+    
     num_skip_tokens = len(tokenizer.encode(prompt_text))
-    logger.info('skipping tokens:', num_skip_tokens)
-    print('skipping tokens:', num_skip_tokens)
+    print('Skipping tokens:', num_skip_tokens)
 
     log_sums = np.zeros(len(texts))
     model.eval()
@@ -70,7 +99,7 @@ def get_log_sum(model, texts, prompt_text):
         # computing one by one because different tokenizers may tokenize text into different number of tokens
         # padding or truncation may reduce the accuracy
         for i, text in enumerate(texts): 
-            inputs = tokenizer(text, return_tensors="pt").to(model.device)
+            inputs = tokenizer(prompt_text + text, return_tensors="pt").to(model.device)
             outputs = model(**inputs)
             logits = outputs.logits[0]
             log_probs = torch.log_softmax(logits, dim=-1)
@@ -78,30 +107,58 @@ def get_log_sum(model, texts, prompt_text):
             for j, token in enumerate(inputs['input_ids'][0][num_skip_tokens:], start=num_skip_tokens): 
                 log_sums[i] += log_probs[j-1][token]
 
+            torch.cuda.empty_cache()
+
     return log_sums
 
 
-def compute_kl(model1, model2, prompt_text, num_samples, seq_length, prompt_text2=None):
+def compute_kl(model1, model2, num_samples, seq_length, 
+               prompt_text1, prompt_text2, 
+               system_text1, system_text2):
 
-    texts = generate(model1, prompt_text, num_seq=num_samples, seq_length=seq_length) 
-    kls = get_log_sum(model1, texts, prompt_text) - \
-          get_log_sum(model2, texts, prompt_text2 if prompt_text2 is not None else prompt_text)
+    texts = generate(model1, 
+                     prompt_text=prompt_text1, 
+                     num_seq=num_samples, 
+                     seq_length=seq_length,
+                     system_text=system_text1) 
+    kls = get_log_sum(model1, texts, prompt_text1, system_text1) - \
+          get_log_sum(model2, texts, prompt_text2, system_text2)
     return kls
 
 
 def main(args: Namespace):
-    model1 = AutoModelForCausalLM.from_pretrained(args.model1,
-                                                  load_in_8bit=True if 'llama' in args.model1 else False,
-                                                  device_map='auto')
-    model2 = AutoModelForCausalLM.from_pretrained(args.model2,
-                                                  load_in_8bit=True if 'llama' in args.model2 else False,
-                                                  device_map='auto')
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    model1 = AutoModelForCausalLM.from_pretrained(
+        args.model1,
+        quantization_config=quantization_config if 'llama' in args.model1.lower() else False,
+        device_map='auto',
+        # takes more time but requires less memory
+        use_cache=False,
+    )
+    if args.model2 != args.model1:
+        model2 = AutoModelForCausalLM.from_pretrained(
+            args.model2,
+            quantization_config=quantization_config if 'llama' in args.model2.lower() else False,
+            device_map='auto',
+            # takes more time but requires less memory
+            use_cache=False
+        )
+    else: 
+        model2 = model1
 
     kls = compute_kl(model1, model2,
-                     prompt_text=args.prompt, 
-                     prompt_text2=args.prompt2,
                      num_samples=args.num_samples,
-                     seq_length=args.seq_length)
+                     seq_length=args.seq_length,
+                     prompt_text1=args.prompt1, 
+                     prompt_text2=args.prompt2,
+                     system_text1=args.system1,
+                     system_text2=args.system2)
     
     data = {
         'model1': args.model1,
@@ -109,8 +166,10 @@ def main(args: Namespace):
         'seq_length': args.seq_length,
         'num_samples': args.num_samples,
         'kls': kls.tolist(),
-        'prompt': args.prompt,
-        'prompt2': args.prompt2
+        'prompt1': args.prompt1,
+        'prompt2': args.prompt2,
+        'system1': args.system1,
+        'system2': args.system2
     }
 
     current_GMT = time.gmtime()
